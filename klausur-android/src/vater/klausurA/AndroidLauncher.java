d73daaf0c44937f4e4bb47a0ea620311f49f4dde
Markus Vater <markusvat@gmail.com>
2015-12-29 17:43:14 +0100

    0.007

--- klausur-android/src/vater/klausurA/AndroidLauncher.java
+++ klausur-android/src/vater/klausurA/AndroidLauncher.java
@@ -1,16 +1,15 @@
 package vater.klausurA;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import vater.klausur.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
+	/*@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
-	}
+	}*/
 }
+//The type android.os.Handler cannot be resolved. It is indirectly referenced from required .class files	AndroidLauncher.java	/klausur-android/src/vater/klausurA	line 1	Java Problem
